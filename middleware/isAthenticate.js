8cd906748b8eeca53b7ad9b5290a6c67f4859816
subhradwip01 <subhradwip01@gmail.com>
2022-04-30 22:53:30 +0530

    authentication middlewere added
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ middleware/isAthenticate.js
@@ -0,0 +1,26 @@
+const jwt=require("jsonwebtoken")
+
+const isAthenticate=async (req,res,next)=>{
+    // Get the auth token
+    const authToken=req.get("Authorization");
+    if(!authToken){
+        res.status(401).json({
+            message:"Authentication tokn not available"
+        })
+    } 
+
+    const token=authToken.split(" ")[1];
+    jwt.verify(token,process.env.PRIVATE_KEY,(err,userInfo)=>{
+        if(err){
+            res.status(401).json({
+                message:"Not a valid Auth token"
+            })
+        }
+        req.userInfo=userInfo
+        next()
+    })
+    
+}
+
+module.exports=isAthenticate
+    
