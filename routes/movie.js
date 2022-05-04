17edf8c40a8ef1b055460da973778b2bc0c10770
subhradwip01 <subhradwip01@gmail.com>
2022-05-04 22:19:21 +0530

    movie route bug fix
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- routes/movie.js
+++ routes/movie.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const isAthenticate=require("../middleware/isAthenticate")
 const movieController=require("../controllers/movie")
 // Post new movie
-router.post("/add".isAthenticate,movieController.postNewMovie)
+router.post("/add",isAthenticate,movieController.postNewMovie)
 // Post Update of movie
 router.put("/:movieId",isAthenticate,movieController.postUpdateMovie)
 // Get detils of a movie
@@ -11,7 +11,7 @@ router.get("/find/:movieId",isAthenticate,movieController.getMovieDetails)
 // Delete movie
 router.delete("/:movieId",isAthenticate,movieController.postDeleteMovie)
 // get all movies
-router.get("/",isAthenticate,movieController,movieController.getMovies)
-router.get("/random",isAthenticate,movieController,movieController.getRandomMovie)
+router.get("/",isAthenticate,movieController.getMovies)
+router.get("/random",isAthenticate,movieController.getRandomMovie)
 
 module.exports=router
